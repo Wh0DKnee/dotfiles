82f4e3f595c0da8b929dbc1396b500849105e8d0
Simon <simonr1337@gmail.com>
2024-08-18 20:55:07 +0200

    add more evil helper functions, add eshell async command

--- doom/config.el
+++ doom/config.el
@@ -1,17 +1,27 @@
 (setq   display-line-numbers-type       'relative
         org-directory                   "~/org/"
         orderless-component-separator   #'orderless-escapable-split-on-space
-        orderless-matching-styles       '(orderless-flex)
-)
+        orderless-matching-styles       '(orderless-flex))
 
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
 (add-hook 'window-setup-hook #'treemacs)
 
-(evil-define-operator sr/evil-delete-to-void-register (beg end type register)
+(evil-define-operator sr/evil-delete-to-void-register (beg end type _register yank-handler)
   "Delete text from BEG to END with TYPE, placing it in the void register."
-  :move-point nil
-  :type exclusive
-  (evil-delete beg end type ?_))
+  (interactive "<R><x><y>")
+  (evil-delete beg end type ?_ yank-handler))
+
+(defun sr/evil-delete-to-end-of-line-to-void-register ()
+  "Delete from the point to the end of the line into the void register."
+  (interactive)
+  (evil-delete (point) (line-end-position) 'inclusive ?_))
+
+(defun sr/delete-char-to-void-register ()
+  "Delete the character under the cursor and place it in the void register."
+  (interactive)
+  (let ((beg (point))
+        (end (1+ (point)))) ;; end is one character after the point
+    (evil-delete beg end 'exclusive ?_)))
 
 (defun sr/org-insert-code-block ()
   "Insert a code block with the specified LANG."
@@ -20,13 +30,19 @@
     (insert (format "#+begin_src %s\n\n#+end_src\n" lang))
     (previous-line 2)))  ;; Move cursor between the source block lines
 
-;; I have this bound to a custom key combo on my QMK keyboard
+(defun eshell/async (&rest args)
+  "Concatenate all ARGS into a single string and pass that string
+   to async-shell-command."
+  (async-shell-command (string-join args " ")))
+
 (map! :map 'override "C-M-s-<f12>" #'execute-extended-command)
 
 (map! :leader
       :desc "Consult buffer" "," #'consult-buffer)
 
 (map! :n "r" #'sr/evil-delete-to-void-register)
+(map! :n "R" #'sr/evil-delete-to-end-of-line-to-void-register)
+(map! :n "x" #'sr/delete-char-to-void-register)
 
 (use-package! dired-subtree
   :after dired)
@@ -35,6 +51,11 @@
   :after vertico
   :custom
   (completion-styles '(orderless)))
+
+(after! evil
+  (defalias #'forward-evil-word #'forward-evil-symbol)
+  ;; make evil-search-word look for symbol rather than word boundaries
+  (setq-default evil-symbol-word-search t))
 
 (add-to-list 'display-buffer-alist
              '("\\*Async Shell Command\\*"
--- doom/custom.el
+++ doom/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-types '((initialization) (defvaralias))))
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
+ '(warning-suppress-types '((initialization) (defvaralias)) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
